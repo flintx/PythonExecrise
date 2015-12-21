00ce93ae9d9b61ba78e50849ac2f9b0e76047565
flintx <940852578@qq.com>
2015-12-21 21:33:22 +0800

    add 0004

---
+++ 0004/0004.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+# coding : utf-8
+
+
+import re
+
+def CountWord(file):
+	f = open(file, 'r')
+	text = f.read()
+	pa = "[a-zA-Z0-9]+"
+	words = re.findall(pa, text)
+	return len(words)
+
+def main():
+	print(CountWord("Gettysburg Address.txt"))
+
+if __name__ == '__main__':
+	main()
